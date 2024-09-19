dc595430d6ca01c224395803bd00e742c381ba57
abir-g <abir.gupta2005@gmail.com>
2024-09-20 01:19:16 +0530

    Created initial models for Newscards and categories, and basic admin panel

--- newsprovider/admin.py
+++ newsprovider/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Newscard, Category
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Columns to display in the list view
+    search_fields = ('name',)  # Fields to search by
+
+@admin.register(Newscard)
+class NewscardAdmin(admin.ModelAdmin):
+    list_display = ('title', 'summary', 'link', 'image')  # Columns to display in the list view
+    search_fields = ('title', 'summary', 'link')  # Fields to search by
+    list_filter = ('categories',)  # Filter by categories
+    filter_horizontal = ('categories',)  # Add a horizontal filter widget for many-to-many fields
--- newsprovider/models.py
+++ newsprovider/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Newscard(models.Model):
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    image = models.ImageField(upload_to='news_images/')
+    link = models.URLField(max_length=500)
+    categories = models.ManyToManyField(Category, related_name='newscards')  # Many-to-many relationship
+
+    def __str__(self):
+        return self.title
