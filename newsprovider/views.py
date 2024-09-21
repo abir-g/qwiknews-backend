1a09c250dd332db0088943e616808f050005c6e8
abir-g <abir.gupta2005@gmail.com>
2024-09-22 01:02:09 +0530

    Added the required mixins to the core app

--- newsprovider/views.py
+++ newsprovider/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import mixins
+from rest_framework import viewsets
 
 from .serializers import NewsCardSerializer
 from .models import NewsCard
@@ -7,7 +9,11 @@ from .pagination import DefaultPagination
 # Create your views here.
 
 
-class NewsCardViewSet(ReadOnlyModelViewSet):
+class NewsCardViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
     queryset = NewsCard.objects.all()
     serializer_class = NewsCardSerializer
     pagination_class = DefaultPagination
