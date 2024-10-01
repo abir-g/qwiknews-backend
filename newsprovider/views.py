6deaa223a59368360d657d114cda1416a4551d75
abir-g <abir.gupta2005@gmail.com>
2024-10-02 00:05:37 +0530

    Added search by category name and validation error if both category_name and id are passed

--- newsprovider/views.py
+++ newsprovider/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+
 
 from .serializers import NewsCardSerializer
 from .models import NewsCard
@@ -17,16 +20,25 @@ class NewsCardViewSet(mixins.ListModelMixin,
 
     def get_queryset(self):
         queryset = NewsCard.objects.filter(is_flagged=0).order_by('-timestamp')
+        
+        # Get query parameters
+        category_id = self.request.query_params.get('category_id')
+        category_name = self.request.query_params.get('category_name')
+        title_search = self.request.query_params.get('title_search')
 
-        # Get the category_id and title search parameter from the URL query parameters
-        category_id = self.kwargs.get('category_id')
-        title_search = self.request.query_params.get('title', None)
+        # Check if both parameters are provided
+        if category_id and category_name:
+            raise ValidationError("Please provide only one of 'category_id' or 'category_name', not both.")
 
-        # Filter by category if provided
+        # Filter by category_id if provided
         if category_id:
             queryset = queryset.filter(categories__id=category_id)
 
-        # Filter by title search if provided
+        # Filter by category_name if provided
+        if category_name:
+            queryset = queryset.filter(categories__name__icontains=category_name)
+
+        # Filter by title_search if provided
         if title_search:
             queryset = queryset.filter(title__icontains=title_search)
 
